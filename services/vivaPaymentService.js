8bdeacfc58317502df242e3f18fb93c2cfc78772
judejudo <86878327+judejudo@users.noreply.github.com>
2024-08-07 16:56:16 +0300

    currencycode

--- services/vivaPaymentService.js
+++ services/vivaPaymentService.js
@@ -49,6 +49,7 @@ class VivaPaymentService {
                         requestLang: customerDetails.requestLang,
                     },
                     paymentNotification: true,
+                    currencyCode: 840,
                 },
                 {
                     headers: {
